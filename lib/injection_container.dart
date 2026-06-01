724daf8732b3d74e2abfb4478c405feb86a8b48a
Varun Kushwah <varunkushwah500notes@gmail.com>
2026-06-02 00:54:27 +0530

    feat(music): included dependancy injection for MusicRepository,YouTubeRemoteDataSource

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,11 +1,26 @@
 import 'package:get_it/get_it.dart';
+
+// Auth Imports
 import 'package:carbon_music/features/auth/domain/auth_repository.dart';
 import 'package:carbon_music/features/auth/data/auth_repository_impl.dart';
+
+// Music Imports
+import 'package:carbon_music/features/music/data/datasources/youtube_remote_data_source.dart';
+import 'package:carbon_music/features/music/domain/repositories/music_repository.dart';
+import 'package:carbon_music/features/music/data/repositories/music_repository_impl.dart';
 
 final sl = GetIt.instance; 
 
 Future<void> init() async {
-
+ //auth
   sl.registerLazySingleton<AuthRepository>(() => AuthRepositoryImpl());
+
  
+  sl.registerLazySingleton<YouTubeRemoteDataSource>(
+    () => YoutubeRemoteDataSourceImp(),
+  );
+
+  sl.registerLazySingleton<MusicRepository>(
+    () => MusicRepositoryImpl(remoteDataSource: sl()),
+  );
 }
